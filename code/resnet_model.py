498e52c005b449e349d433823e8691cefb7689ab
Nidhi Ilanthalaivan <nidhi.ilan@gmail.com>
2023-07-25 15:18:17 -0400

    same message as prior

--- code/resnet_model.py
+++ code/resnet_model.py
@@ -180,12 +180,12 @@ class ResNet(nn.Module):
     with torch.no_grad():
       x = self._conv('conv1', x, 3, in_filter, out_filter, stride)
 
-    with torch.no_grad():
+    with tf.variable_scope('sub2'):
       x = self._batch_norm('bn2', x)
       x = self._relu(x, self.hps.relu_leakiness)
       x = self._conv('conv2', x, 3, out_filter, out_filter, [1, 1, 1, 1])
 
-    with tf.variable_scope('sub_add'):
+    with torch.no_grad():
       if in_filter != out_filter:
         orig_x = tf.nn.avg_pool(orig_x, stride, stride, 'VALID')
         orig_x = tf.pad(

9a6dc09e0cee45531867825bdb45d95ab512f2ab
Bryann Revilla <revillabryann@gmail.com>
2023-07-26 13:30:19 -0400

    Imports are not needed
    imports are only needed if using python 2, python 3 automatically has
    these functionalities

--- code/cifar10_dataset.py
+++ code/cifar10_dataset.py
@@ -15,9 +15,9 @@
 
 """Provides data for the Cifar10 dataset."""
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+#from __future__ import absolute_import
+#from __future__ import division
+#from __future__ import print_function
 
 import os
 

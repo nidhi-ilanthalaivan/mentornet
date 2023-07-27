5aa14b0fc68c28a1d9ad31330ec113d5c257d77c
Bryann Revilla <revillabryann@gmail.com>
2023-07-26 23:42:56 -0400

    extracting individual samples
    self.data is a NumPy array that contains the raw image data of the
    CIFAR-10 dataset, self.targets is a PyTorch tensor that contains
    the labels (targets) corresponding to each image in the dataset,  the
    index of the sample we want to access in the dataset, img will store the
    image data of the sample at index index, and target will store the
    target label (class label) of the same sample
    Links:
    https://pytorch.org/vision/stable/generated/torchvision.datasets.CIFAR10.html
    https://medium.com/@sergioalves94/deep-learning-in-pytorch-with-cifar-10-dataset-858b504a6b54
    https://www.datascienceweekly.org/tutorials/cifar10-pytorch-load-cifar10-dataset-from-torchvision#:~:text=CIFAR10%20is%20a%20dataset%20consisting,Then%20we%20will%20import%20torchvision.&text=Torchvision%20is%20a%20package%20in,%2C%20datasets%2C%20and%20image%20transformations.
    https://pytorch.org/tutorials/beginner/basics/data_tutorial.html
    https://kaiyangzhou.github.io/deep-person-reid/_modules/torchreid/data/datasets/image/market1501.html

--- code/cifar10_dataset.py
+++ code/cifar10_dataset.py
@@ -68,6 +68,7 @@ class CIFAR10Dataset(torch.utils.data.Dataset):
   def __len__(self): 
     return len(self.data)
   def __getitem__(self, index): 
+    img, target = self.data[index], self.targets[index]
   """Gets a dataset tuple with instructions for reading cifar10.
 
   Args:

dff88fa0317c7dc420da0031cba227bdf7f36444
Nidhi Ilanthalaivan <nidhi.ilan@gmail.com>
2023-07-27 23:52:30 -0400

    these comments explain the transforms messages

--- code/cifar_data_provider.py
+++ code/cifar_data_provider.py
@@ -65,6 +65,7 @@ def provide_resnet_data(dataset_name,
 
   transform_train = transforms.Compose([transforms.RandomCrop(32, paddings = 4), transforms.RandomHorizontalFlip(), transforms.ToTensor(), transforms.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225]),])
   transform_test = transforms.Compose([transforms.Resize((32,32)), transforms.ToTensor(), transforms.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225]), ])
+  ## transforms.RandomHorizontalFlip() - randomly flips input image horizontally with probability of 0.5
   image_size = 32
   if split_name == 'train':
     image = tf.image.resize_image_with_crop_or_pad(image, image_size + 4,

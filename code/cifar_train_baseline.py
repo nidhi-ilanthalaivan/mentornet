b9e4ef0a63c238e8c992866a347aac9ede4e1c04
Bryann Revilla <revillabryann@gmail.com>
2023-07-27 22:35:32 -0400

    optimizer variable
    needed for parameter optimization, it uses the model parameters the
    learning rate from args.learning_rate, and a momentum value of 0.9 as
    arguments

--- code/cifar_train_baseline.py
+++ code/cifar_train_baseline.py
@@ -227,6 +227,8 @@ def train_inception_baseline(max_step_run, args):
   
   #Loss function 
   criterion = nn.CrossEntropyLoss()
+  #optmizer 
+  optimizer = torch.optim.SGD(model.parameters(), lr = args.learning_rate, momentum = 0.9)
 
 def main(_):
   os.environ['CUDA_VISIBLE_DEVICES'] = FLAGS.device_id

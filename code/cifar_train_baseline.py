bed3e2a30d87f27195fa48224b81fddce2706f3b
Bryann Revilla <revillabryann@gmail.com>
2023-07-27 22:10:25 -0400

    torch.optim
     provides various optimization algorithms that can be used to update the
     model's parameters during training

--- code/cifar_train_baseline.py
+++ code/cifar_train_baseline.py
@@ -25,7 +25,7 @@ import inception_model
 import resnet_model
 import torch 
 import torch.nn as nn
-import torch.optim as optim 
+import torch.optim 
 import tensorboard
 import torchvision.transforms as transforms 
 from torch.utils.tensorboard import SummaryWriter 

0e53c357f890fe62e7759c096cbc0b0502259851
Bryann Revilla <revillabryann@gmail.com>
2023-07-27 22:48:22 -0400

    logits variable
    the output of the model, to obtain the logits, input images are passed
    through the model

--- code/cifar_train_baseline.py
+++ code/cifar_train_baseline.py
@@ -235,6 +235,8 @@ def train_inception_baseline(max_step_run, args):
   for epoch in range(args.num_epochs): 
     for step, (images, labels) in enumerate(cifar_dataset):
       optimizer.zero_grad()
+    #Foward pass
+      logits = model(images)
 
 def main(_):
   os.environ['CUDA_VISIBLE_DEVICES'] = FLAGS.device_id

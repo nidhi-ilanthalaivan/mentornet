4c448bd59eeef0b51de48afcda981e6db214daf5
Junyao Chen <junyao.chen@nyu.edu>
2023-08-03 22:19:43 -0400

    Refactor data providor
    Future dataset loading can be put in cifar_data_providor.py

--- code/cifar_data_provider.py
+++ code/cifar_data_provider.py
@@ -15,142 +15,46 @@
 
 """Contains code for loading and preprocessing the CIFAR data."""
 
-import cifar100_dataset
 import cifar10_dataset
 import torch
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader, random_split
 import torch.nn as nn
+import torchvision
 
-datasets_map = {
-    'cifar10': cifar10_dataset,
-    'cifar100': cifar100_dataset,
-}
+def provide_cifarnet_data(train_or_test, batch_size):
+  """Provides batches of CIFAR images for cifarnet.""" 
 
-
-def provide_resnet_data(dataset_name,
-                        split_name,
-                        batch_size,
-                        dataset_dir=None,
-                        num_epochs=None):
-  """Provides batches of CIFAR images for resnet.
-
-  Args:
-    dataset_name: Eiether 'cifar10' or 'cifar100'.
-    split_name: Either 'train' or 'test'.
-    batch_size: The number of images in each batch.
-    dataset_dir: The directory where the MNIST data can be found.
-    num_epochs: The number of times each data source is read. If left as None,
-      the data will be cycled through indefinitely.
-
-  Returns:
-    images: A `Tensor` of size [batch_size, 32, 32, 1]
-    one_hot_labels: A `Tensor` of size [batch_size, NUM_CLASSES], where
-      each row has a single element set to one and the rest set to zeros.
-    num_samples: The number of total samples in the dataset.
-    num_classes: The number of total classes in the dataset.
-
-
-  Raises:
-    ValueError: If `split_name` is not either 'train' or 'test'.
-  """
-  dataset = get_dataset(dataset_name, split_name, dataset_dir=dataset_dir)
-
-  # num_epochs = 1 if split_name == 'test' else None
-  
-  if dataset_name == 'cifar100':
-    image_key, label_key = 'image', 'fine_label'
-  else:
-    image_key, label_key = 'image', 'label'
-
-  transform_train = transforms.Compose([transforms.RandomCrop(32, paddings = 4), transforms.RandomHorizontalFlip(), transforms.ToTensor(), transforms.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225]),])
-  transform_test = transforms.Compose([transforms.Resize((32,32)), transforms.ToTensor(), transforms.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225]), ])
   ## transforms.RandomCrop(32, padding = 4) performs a random crop of size 32x32 pixels on imput and the image is first zero padded with a 4 pixel border 
   ## transforms.RandomHorizontalFlip() - randomly flips input image horizontally with probability of 0.5
   ## transforms.Resize((32,32)) - resizes input image to fixed size of 32x32 pizels and its used for test data to ensure that all test images have the same size as training images
   ## transforms.ToTensor() - converts input image to a pytorch tensor 
-  ##transforms.Normalize(mean = [0.485...]) normalizes tensor by subracts mean and dividing by the standard deviation 
+  ## transforms.Normalize(mean = [0.485...]) normalizes tensor by subracts mean and dividing by the standard deviation 
+    
   image_size = 32
-  if split_name == 'train':
-    transform = transform_train
-  else:
-    transform = transform_test
+  transform = transforms.Compose([transforms.RandomCrop(image_size, padding = 4), 
+                                  transforms.RandomHorizontalFlip(),
+                                  transforms.ToTensor(), 
+                                  transforms.Normalize(mean = [0.485, 0.456, 0.406], 
+                                                      std = [0.229,0.224,0.225]),])
 
-  # Creates a QueueRunner for the pre-fetching operation.
-  images = [transform(image) for image in dataset[image_key]]
-  labels = dataset[label_key]
-  one_hot_labels = torch.nn.functional.one_hot(labels, dataset.num_classes)
-  one_hot_labels = torch.squeeze(one_hot_labels, 1)
-  return images, one_hot_labels, len(dataset), dataset.num_classes
-
-
-def provide_cifarnet_data(dataset_name,
-                          split_name,
-                          batch_size,
-                          dataset_dir=None,
-                          num_epochs=None):
-  """Provides batches of CIFAR images for cifarnet.
-
-  Args:
-    dataset_name: Eiether 'cifar10' or 'cifar100'.
-    split_name: Either 'train' or 'test'.
-    batch_size: The number of images in each batch.
-    dataset_dir: The directory where the MNIST data can be found.
-    num_epochs: The number of times each data source is read. If left as None,
-      the data will be cycled through indefinitely.
-
-  Returns:
-    images: A `Tensor` of size [batch_size, 32, 32, 1]
-    one_hot_labels: A `Tensor` of size [batch_size, NUM_CLASSES], where
-      each row has a single element set to one and the rest set to zeros.
-    num_samples: The number of total samples in the dataset.
-    num_classes: The number of total classes in the dataset.
-
-  Raises:
-    ValueError: If `split_name` is not either 'train' or 'test'.
-  """
-  dataset = get_dataset(dataset_name, split_name, dataset_dir=dataset_dir)
-  # num_epochs = 1 if split_name == 'test' else None
-  
-
-  if dataset_name == 'cifar100':
-    image_key, label_key = 'image', 'fine_label'
-  else:
-    image_key, label_key = 'image', 'label'
-
-  image_size = 32
-  
   # preprocess the images.
-  if split_name == 'train':
-    transform = transforms.Compose([transforms.RandomCrop(image_size, padding = 4), transforms.RandomHorizontalFlip(),transforms.ToTensor(), transforms.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229,0.224,0.225]),])
+  if train_or_test == 'train':
+    transform = transforms.Compose([
+      transforms.RandomCrop(image_size, padding = 4), 
+      transforms.RandomHorizontalFlip(),
+      transforms.ToTensor(), 
+      transforms.Normalize(mean = [0.485, 0.456, 0.406], 
+                           std = [0.229,0.224,0.225]),])
+    train_dataset = torchvision.datasets.CIFAR10(root=str('./data') , train=True, download=True, transform=transform)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    return train_loader
   else:
-    transform = transforms.Compose([transforms.Resize((image_size, image_size)),transforms.ToTensor(), transforms.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229,0.224, 0.225]),])
-
-  # Creates a QueueRunner for the pre-fetching operation.
-  images = [transform(image) for image in dataset[image_key]]
-  labels = dataset[label_key]
-  one_hot_labels = torch.nn.functional.one_hot(labels, dataset.num_classes)
-  one_hot_labels = torch.squeeze(one_hot_labels, 1)
-  return images, one_hot_labels, len(dataset), dataset.num_classes
-
-
-def get_dataset(name, split_name, **kwargs):
-  """Given a dataset name and a split_name returns a Dataset.
-
-  Args:
-    name: String, name of the dataset.
-    split_name: A train/test split name.
-    **kwargs: Extra kwargs for get_split, for example dataset_dir.
-
-  Returns:
-    A `Dataset` namedtuple.
-
-  Raises:
-    ValueError: if dataset unknown.
-  """
-  if name not in datasets_map:
-    raise ValueError('Name of dataset unknown %s' % name)
-  dataset = datasets_map[name].get_split(split_name, **kwargs)
-  dataset.name = name
-  return dataset
-
+    transform = transforms.Compose([
+      transforms.Resize((image_size, image_size)),
+      transforms.ToTensor(), 
+      transforms.Normalize(mean = [0.485, 0.456, 0.406], 
+                           std = [0.229,0.224, 0.225]),])
+    test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    return test_loader

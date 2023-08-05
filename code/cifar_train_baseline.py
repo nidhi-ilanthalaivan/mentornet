91244b1d52573683b45caa18124fe31f081f473a
Bryann Revilla <revillabryann@gmail.com>
2023-08-04 21:46:00 -0400

    Merge branch 'master' of https://github.com/nidhi-ilanthalaivan/mentornet

--- code/cifar_train_baseline.py
+++ code/cifar_train_baseline.py
@@ -41,7 +41,7 @@ FLAGS = {
   'num_epochs': 40,
   'num_epochs_per_decay': 10, #'Number of epochs after which learning rate decays.
   'save_summaries_secs': 120, # The frequency with which summaries are saved, in seconds.
-  'save_interval_secs': 1200, # The frequency with which the model is saved, in seconds.
+  'save_interval_secs': 1200# The frequency with which the model is saved, in seconds.
   'dropout': 0.2,
   'checkpoint_dir': "",
 }
@@ -89,7 +89,6 @@ def train_inception_baseline():
         inputs, labels = data[0].to(device), data[1].to(device)
 
         optimizer.zero_grad()
-
         outputs = model(inputs)          
         loss = criterion(outputs, labels)
         loss.backward()
